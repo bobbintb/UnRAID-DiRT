a11c50be38359eb7d5b6773669f611b7df59b5d1
bobbintb <bobbintb@gmail.com>
2024-06-26 08:22:59 -0600

    Update pre-install.sh

--- sh/pre-install.sh
+++ sh/pre-install.sh
@@ -1,8 +1,8 @@
 echo "-----------------------------------------------------------"
 echo "Installing dependencies..."
 echo "-----------------------------------------------------------"
-NODE="https://slackware.uk/slackware/slackware64-current/slackware64/l/nodejs-20.14.0-x86_64-1.txz"
 FILE="nodejs-20.14.0-x86_64-1.txz"
+NODE="https://slackware.uk/slackware/slackware64-current/slackware64/l/${FILE}"
 if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
     wget "$NODE" -O "/boot/config/plugins/&name;/$FILE"
 fi
