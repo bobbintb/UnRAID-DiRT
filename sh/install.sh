e9a6bfe64815ec8ff95d02dabfd27187afeeb56c
bobbintb <bobbintb@gmail.com>
2024-06-26 08:24:13 -0600

    Delete sh/install.sh

--- sh/install.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/nodejs-20.11.0-x86_64-1_SBo_UES.txz
-installpkg nodejs-20.11.0-x86_64-1_SBo_UES.txz
-rm nodejs-20.11.0-x86_64-1_SBo_UES.txz
