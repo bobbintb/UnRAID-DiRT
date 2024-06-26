b42a6ee61df82132916502ed66d8085d6b7ab22e
bobbintb <bobbintb@gmail.com>
2024-06-26 08:20:45 -0600

    Add files via upload

---
+++ sh/install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/nodejs-20.11.0-x86_64-1_SBo_UES.txz
+installpkg nodejs-20.11.0-x86_64-1_SBo_UES.txz
+rm nodejs-20.11.0-x86_64-1_SBo_UES.txz
