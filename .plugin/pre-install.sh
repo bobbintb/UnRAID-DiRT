311d51f928ba3f9c7f4d56009e71738f495c7f57
bobbintb <bobbintb@gmail.com>
2024-06-27 13:58:14 -0600

    Update pre-install.sh

--- .plugin/pre-install.sh
+++ .plugin/pre-install.sh
@@ -1,9 +1,18 @@
 echo "-----------------------------------------------------------"
-echo "Installing dependencies..."
+echo "Checking dependencies..."
 echo "-----------------------------------------------------------"
 FILE="nodejs-20.11.0-x86_64-1_SBo_UES.txz"
 NODE="https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
 if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
+    echo "-----------------------------------------------------------"
+    echo "Downloading NodeJS..."
+    echo "-----------------------------------------------------------"
     wget "$NODE" -O "/boot/config/plugins/&name;/$FILE"
 fi
-installpkg "/boot/config/plugins/&name;/$FILE"
+if ! command -v node &> /dev/null; then
+    echo "-----------------------------------------------------------"
+    echo "Installing NodeJS..."
+    echo "-----------------------------------------------------------"
+    installpkg "/boot/config/plugins/&name;/$FILE"
+fi
+echo "Done."
