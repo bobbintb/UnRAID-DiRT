71fcbb0f72dfd1257b38a833e3bf784f96f32e24
Armando <bobbintb@gmail.com>
2024-09-12 09:44:40 -0600

    Updated 02-pre-install.sh. Cleaned it up to be easier to read and modify.

--- .plugin/02-pre-install.sh
+++ .plugin/02-pre-install.sh
@@ -3,8 +3,8 @@ echo "Checking dependencies..."
 echo "-----------------------------------------------------------"
 
 # The first three arguments MUST be supplied in the order of NAME, FILE, and URL. For example:
-# install_package "nodejs"\
-# "nodejs-20.11.0-x86_64-1_SBo_UES.txz"\
+# install_package "nodejs" \
+# "nodejs-20.11.0-x86_64-1_SBo_UES.txz" \
 # "https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
 # This is for a normal installation of installing a *.txz file. You can supply a 4th argument
 # if you need to just move a file to install it.
@@ -36,37 +36,37 @@ install_package() {
     fi
 }
 
-install_package "nodejs"\
-"nodejs-20.11.0-x86_64-1_SBo_UES.txz"\
+install_package "nodejs" \
+"nodejs-20.11.0-x86_64-1_SBo_UES.txz" \
 "https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
 
-install_package "audit"\
-"audit-4.0.1-x86_64-4cf.txz"\
+install_package "audit" \
+"audit-4.0.1-x86_64-4cf.txz" \
 "https://slackers.it/repository/slackware64-current/audit/${FILE}"
 
-install_package "protobuf"\
-"protobuf-3.19.6-x86_64-1gds.txz"\
+install_package "protobuf" \
+"protobuf-3.19.6-x86_64-1gds.txz" \
 "https://ftp.sotirov-bg.net/pub/contrib/slackware/packages/slackware64-15.0/${FILE}"
 
-install_package "laurel"\
-"laurel-v0.6.3-x86_64-glibc.tar.gz"\
+install_package "laurel" \
+"laurel-v0.6.3-x86_64-glibc.tar.gz" \
 "https://github.com/threathunters-io/laurel/releases/download/v0.6.3/${FILE}"
 
 # install_package "redis"\
 # "redis-7.4.0-x86_64-1loom.txz"\
 # "https://github.com/bobbintb/Slackware_Packages/raw/main/${NAME}/${FILE}"
 
-install_package "keydb"\
-"keydb-6.3.4-x86_64-1loom.txz"\
+install_package "keydb" \
+"keydb-6.3.4-x86_64-1loom.txz" \
 "https://github.com/bobbintb/Slackware_Packages/raw/main/keydb/${FILE}"
 
-install_package "openssl"\
-"openssl-1.1.1m-x86_64-1.txz"\
+install_package "openssl" \
+"openssl-1.1.1m-x86_64-1.txz" \
 "https://slackware.uk/slackware/slackware64-15.0/slackware64/n/${FILE}"
 
-install_package "redisearch"\
-"redisearch.so"\
-"https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/${FILE}"\
+install_package "redisearch" \
+"redisearch.so" \
+"https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/${FILE}" \
 "/opt/keydb/lib/"
 sed -i '54 i\loadmodule /opt/keydb/lib/redisearch.so' /etc/keydb/keydb.conf
 
