07c55c89f57abbc39bcaa7c800ef2a4b629e4ea9
Armando <bobbintb@gmail.com>
2024-09-12 09:37:30 -0600

    Updated 02-pre-install.sh. Cleaned it up to be easier to read and modify.

--- .plugin/02-pre-install.sh
+++ .plugin/02-pre-install.sh
@@ -1,119 +1,73 @@
 echo "-----------------------------------------------------------"
 echo "Checking dependencies..."
 echo "-----------------------------------------------------------"
-FILE="nodejs-20.11.0-x86_64-1_SBo_UES.txz"
-URL="https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
-NAME="nodejs"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if ! command -v $NAME >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    installpkg "/boot/config/plugins/&name;/$FILE"
-fi
 
-FILE="audit-4.0.1-x86_64-4cf.txz"
-URL="https://slackers.it/repository/slackware64-current/audit/${FILE}"
-NAME="audit"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if ! command -v $NAME >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    installpkg "/boot/config/plugins/&name;/$FILE"
-fi
+# The first three arguments MUST be supplied in the order of NAME, FILE, and URL. For example:
+# install_package "nodejs"\
+# "nodejs-20.11.0-x86_64-1_SBo_UES.txz"\
+# "https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
+# This is for a normal installation of installing a *.txz file. You can supply a 4th argument
+# if you need to just move a file to install it.
 
-FILE="protobuf-3.19.6-x86_64-1gds.txz"
-URL="https://ftp.sotirov-bg.net/pub/contrib/slackware/packages/slackware64-15.0/${FILE}"
-NAME="protobuf"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if ! command -v $NAME >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    installpkg "/boot/config/plugins/&name;/$FILE"
-fi
+install_package() {
+    NAME="$1"
+    FILE="$2"
+    URL="$3"
+    PLUGIN_PATH="/boot/config/plugins/${NAME}/${FILE}"
+    basename="${FILE%.*}"
 
-FILE="openssl-1.1.1m-x86_64-1.txz"
-URL="https://slackware.uk/slackware/slackware64-15.0/slackware64/n/${FILE}"
-NAME="openssl"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if [ ! -f /usr/lib64/libcrypto.so.1.1 ] >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    installpkg "/boot/config/plugins/&name;/$FILE"
-fi
+    if [ ! -f "$PLUGIN_PATH" ]; then
+        echo "-----------------------------------------------------------"
+        echo "Downloading $NAME..."
+        echo "-----------------------------------------------------------"
+        wget "$URL" -O "$PLUGIN_PATH"
+    fi
 
-FILE="laurel-v0.6.3-x86_64-glibc.tar.gz"
-URL="https://github.com/threathunters-io/laurel/releases/download/v0.6.3/${FILE}"
-NAME="laurel"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if ! command -v $NAME >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    tar xzf "/boot/config/plugins/&name;/$FILE" -C "/boot/config/plugins/&name;/" laurel
-    rm "/boot/config/plugins/&name;/$FILE"
-    install -m755 "/boot/config/plugins/&name;/laurel" /usr/local/sbin/laurel
-fi
+    if [ ! -f "/var/log/packages/${basename}" ] >/dev/null 2>&amp;1; then
+        echo "-----------------------------------------------------------"
+        echo "Installing $NAME..."
+        echo "-----------------------------------------------------------"
 
-FILE="redis-7.4.0-x86_64-1loom.txz"
-URL="https://github.com/bobbintb/Slackware_Packages/raw/main/${NAME}/${FILE}"
-NAME="redis"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if ! command -v $NAME >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    installpkg "/boot/config/plugins/&name;/$FILE"
-fi
+        if [ -n "$4" ]; then
+                cp "/boot/config/plugins/&name;/$FILE" "$4"
+            else
+                installpkg "$PLUGIN_PATH"
+            fi
+    fi
+}
 
-FILE="redisearch.so"
-URL="https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/${FILE}"
-NAME="redisearch"
-if [ ! -f "/boot/config/plugins/&name;/$FILE" ]; then
-    echo "-----------------------------------------------------------"
-    echo "Downloading $NAME..."
-    echo "-----------------------------------------------------------"
-    wget "$URL" -O "/boot/config/plugins/&name;/$FILE"
-fi
-if [ ! -f /opt/keydb/lib/$FILE ] >/dev/null 2>&amp;1; then
-    echo "-----------------------------------------------------------"
-    echo "Installing $NAME..."
-    echo "-----------------------------------------------------------"
-    cp "/boot/config/plugins/&name;/$FILE" /opt/keydb/lib/
-fi
+install_package "nodejs"\
+"nodejs-20.11.0-x86_64-1_SBo_UES.txz"\
+"https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
+
+install_package "audit"\
+"audit-4.0.1-x86_64-4cf.txz"\
+"https://slackers.it/repository/slackware64-current/audit/${FILE}"
+
+install_package "protobuf"\
+"protobuf-3.19.6-x86_64-1gds.txz"\
+"https://ftp.sotirov-bg.net/pub/contrib/slackware/packages/slackware64-15.0/${FILE}"
+
+install_package "laurel"\
+"laurel-v0.6.3-x86_64-glibc.tar.gz"\
+"https://github.com/threathunters-io/laurel/releases/download/v0.6.3/${FILE}"
+
+# install_package "redis"\
+# "redis-7.4.0-x86_64-1loom.txz"\
+# "https://github.com/bobbintb/Slackware_Packages/raw/main/${NAME}/${FILE}"
+
+install_package "keydb"\
+"keydb-6.3.4-x86_64-1loom.txz"\
+"https://github.com/bobbintb/Slackware_Packages/raw/main/keydb/${FILE}"
+
+install_package "openssl"\
+"openssl-1.1.1m-x86_64-1.txz"\
+"https://slackware.uk/slackware/slackware64-15.0/slackware64/n/${FILE}"
+
+install_package "redisearch"\
+"redisearch.so"\
+"https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/${FILE}"\
+"/opt/keydb/lib/"
 sed -i '54 i\loadmodule /opt/keydb/lib/redisearch.so' /etc/keydb/keydb.conf
 
 echo "Done."
