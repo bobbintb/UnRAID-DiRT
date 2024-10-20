d30223ad42e0b42211edaa19c322556156a7f323
Armando <bobbintb@gmail.com>
2024-10-19 22:06:41 -0600

    stuff

--- nodejs/processDuplicates.js
+++ nodejs/processDuplicates.js
@@ -1,6 +1,21 @@
 import {process, queue} from "./redisHelper.js"
 import fs from "fs";
+import CryptoJS from 'crypto-js';
 
+export function enqueueFileAction(action, src) {
+    const jobData = {
+        action: action,
+        src: src
+    };
+    process.createJob(jobData)
+        .setId(CryptoJS.MD5(src).toString())
+        .save()
+        .then(() => {
+        })
+        .catch(err => {
+            console.error(`Failed to add job ${jobId}:`, err);
+        });
+}
 export function enqueueFileAction(action, src) {
     const jobData = {
         action: action,
