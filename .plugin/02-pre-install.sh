423a48ba077ca2861a6abb6f1dec46688db077d0
Armando <bobbintb@gmail.com>
2024-10-29 09:48:58 -0600

    stuff

--- .plugin/02-pre-install.sh
+++ .plugin/02-pre-install.sh
@@ -3,14 +3,6 @@ echo "-----------------------------------------------------------"
 echo "Checking dependencies for $PLUGIN_NAME..."
 echo "-----------------------------------------------------------"
 
-# The first three arguments MUST be supplied in the order of NAME, FILE, and URL. For example:
-# install_package "nodejs" \
-# "nodejs-20.11.0-x86_64-1_SBo_UES.txz" \
-# "https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/${FILE}"
-# There must be a space after each line before the backslash and the string or else the lines will run together.
-# This is for a normal installation of installing a *.txz file. You can supply a 4th argument
-# if you need to just move a file to install it.
-
 install_package() {
     NAME="$1"
     URL="$2"
@@ -18,7 +10,6 @@ install_package() {
     BASE_URL=$(dirname "$URL")/
     TXZ_PATH="/boot/config/plugins/${PLUGIN_NAME}/${FILE}"
     FILE_BASE="${FILE%.*}"
-
     if [ ! -f "$TXZ_PATH" ]; then
         echo "-----------------------------------------------------------"
         echo "$FILE not cached. Downloading $NAME..."
@@ -26,9 +17,7 @@ install_package() {
         if ! wget --spider "$URL" 2>/dev/null; then
             echo "  File $FILE not found. Searching for"
             echo "  .txz files in $BASE_URL..."
-            curl "$URL" --create-dirs -o "$TXZ_PATH"
-            #wget -q -O "$TXZ_PATH" "$BASE_URL"
-            FIRST_FILE=$(awk -F'href="' '/\.txz"/ {print $2; exit}' "$TXZ_PATH" | awk -F'"' '{print $1}')
+            FIRST_FILE=$(wget -q -O - "$BASE_URL" | grep -oE 'href="[^"]+\.txz"' | head -n 1 | cut -d'"' -f2)
             if [ -n "$FIRST_FILE" ]; then
                 echo "  $FILE was not found but $FIRST_FILE was."
                 echo "  The package was likely updated and the old file removed."
@@ -71,6 +60,25 @@ install_package "audit" \
 #install_package "keydb" \
 #"https://github.com/bobbintb/Slackware_Packages/raw/main/keydb/keydb-6.3.4-x86_64-1loom.txz"
 
+install_package "nodejs" \
+"https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/nodejs-20.11.0-x86_64-1_SBo_UES.txz"
+
+install_package "openssl" \
+"https://slackware.uk/slackware/slackware64-15.0/slackware64/n/openssl-1.1.1m-x86_64-1.txz"
+
+install_package "protobuf" \
+"https://ftp.sotirov-bg.net/pub/contrib/slackware/packages/slackware64-15.0/protobuf-3.19.6-x86_64-1gds.txz"
+
+install_package "redis"\
+"https://github.com/bobbintb/Slackware_Packages/raw/main/${NAME}/redis-7.4.0-x86_64-1loom.txz"
+
+mkdir -p "/opt/redis/lib/"
+install_package "redisearch" \
+"https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/redisearch.so" \
+"/opt/redis/lib/"
+
+chmod +x /opt/redis/lib/redisearch.so
+
 NAME="laurel"
 URL="https://github.com/threathunters-io/laurel/releases/download/v0.6.3/laurel-v0.6.3-x86_64-glibc.tar.gz"
 FILE=$(basename "$URL")
@@ -93,24 +101,5 @@ if [ ! -f "/var/log/packages/${FILE_BASE}" ] >/dev/null 2>&amp;1; then
   echo "-----------------------------------------------------------"
   install -m755 "/boot/config/plugins/${PLUGIN_NAME}/${NAME}" /usr/local/sbin/${NAME}
 fi
-
-install_package "nodejs" \
-"https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/nodejs-20.11.0-x86_64-1_SBo_UES.txz"
-
-install_package "openssl" \
-"https://slackware.uk/slackware/slackware64-15.0/slackware64/n/openssl-1.1.1m-x86_64-1.txz"
-
-install_package "protobuf" \
-"https://ftp.sotirov-bg.net/pub/contrib/slackware/packages/slackware64-15.0/protobuf-3.19.6-x86_64-1gds.txz"
-
-install_package "redis"\
-"https://github.com/bobbintb/Slackware_Packages/raw/main/${NAME}/redis-7.4.0-x86_64-1loom.txz"
-
-mkdir -p "/opt/redis/lib/"
-install_package "redisearch" \
-"https://github.com/bobbintb/Slackware_Packages/raw/main/redisearch/2.10.7/redisearch.so" \
-"/opt/redis/lib/"
-
-chmod +x /opt/redis/lib/redisearch.so
 
 echo "Done."
