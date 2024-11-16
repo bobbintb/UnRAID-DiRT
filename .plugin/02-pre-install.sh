068c706d81953c0ee272313ebca55947bbcdc651
Armando <bobbintb@gmail.com>
2024-11-15 21:13:51 -0700

    stuff

--- .plugin/02-pre-install.sh
+++ .plugin/02-pre-install.sh
@@ -64,8 +64,8 @@ install_package() {
     fi
 }
 
-install_package "audit" \
-"https://slackers.it/repository/slackware64-current/audit/audit-4.0.2-x86_64-1cf.txz"
+#install_package "audit" \
+#"https://slackers.it/repository/slackware64-current/audit/audit-4.0.2-x86_64-1cf.txz"
 
 install_package "nodejs" \
 "https://github.com/UnRAIDES/unRAID-NerdTools/raw/main/packages/pkgs/nodejs-20.11.0-x86_64-1_SBo_UES.txz"
@@ -87,7 +87,10 @@ install_package "redisearch" \
 #"https://github.com/bobbintb/Slackware_Packages/raw/main/redisjson/2.8.4/librejson.so" \
 #"/usr/bin/valkey-modules/"
 
-install_package "laurel" \
-"https://github.com/bobbintb/Slackware_Packages/raw/refs/heads/main/laurel/laurel-v0.6.3.txz"
+#install_package "laurel" \
+#"https://github.com/bobbintb/Slackware_Packages/raw/refs/heads/main/laurel/laurel-v0.6.3.txz"
+
+install_package "go-audit" \
+"https://github.com/bobbintb/Slackware_Packages/raw/refs/heads/main/go-audit/go-audit-v1.2.0.txz"
 
 echo "Done."
--- .plugin/03-post-install.sh
+++ .plugin/03-post-install.sh
@@ -8,17 +8,17 @@ echo "-----------------------------------------------------------"
 echo "Configuring..."
 echo "-----------------------------------------------------------"
 
-ln -s /etc/rc.d/rc.auditd /usr/bin/rc.auditd
+#ln -s /etc/rc.d/rc.auditd /usr/bin/rc.auditd
 ln -s /usr/local/etc/rc.d/rc.valkey /etc/rc.d/rc.valkey
 chmod +x /etc/rc.d/rc.valkey
 # ln -s /etc/rc.d/rc.redis /usr/local/etc/rc.d/rc.redis
 # sed -i '54 i\loadmodule /opt/keydb/lib/redisearch.so' /etc/keydb/keydb.conf
 # sed -i '54 i\loadmodule /opt/redis/lib/redisearch.so' /etc/redis/redis.conf
 
-sed -i 's/^write_logs.*/write_logs = no/' /etc/audit/auditd.conf
+#sed -i 's/^write_logs.*/write_logs = no/' /etc/audit/auditd.conf
 
-config_file="/path/to/config.conf"
-new_value="/path/to/your/module.so"
+config_file="/etc/valkey/valkey.conf"
+new_value="/usr/bin/valkey-modules/redisearch.so"
 
 awk -v new_value="$new_value" '
   /^#? *loadmodule / &amp;&amp; !found {
