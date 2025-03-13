5dcc71d38764dd533942f0963375856807b59c53
Armando <bobbintb@gmail.com>
2025-03-12 18:06:16 -0600

    cleanup

--- nodejs/test.js
+++
@@ -1,7 +0,0 @@
-import {fileRepository, processQueue, redis} from "./redisHelper.js";
-
-
-// const isPaused = await processQueue.get;
-await redis.del('dirt:process:og').then(result => {
-    redis.quit();
-})
