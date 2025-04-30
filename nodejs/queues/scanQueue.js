486fec8f4f099d5bf428cccb42d589fdabf4233f
Armando <bobbintb@gmail.com>
2025-04-29 23:04:59 -0600

    stuff

--- nodejs/queues/scanQueue.js
+++ nodejs/queues/scanQueue.js
@@ -34,18 +34,25 @@ export async function addSharesFlow(dirPaths) {
         queueName: 'scanQueue',
         children: [
             {
-                name: 'removeUniques',
+                name: 'saveToRedis',
                 queueName: 'scanQueue',
-                data: {}, // Only needs child_result
+                data: {}, // Will be populated by removeUniques
                 children: [
                     {
-                        name: 'getAllFiles',
+                        name: 'removeUniques',
                         queueName: 'scanQueue',
-                        data: {
-                        input: dirPaths
+                        data: {}, 
+                        children: [
+                            {
+                                name: 'getAllFiles',
+                                queueName: 'scanQueue',
+                                data: {
+                                    input: dirPaths
+                                }
+                            }
+                        ]
                     }
-                }
-            ]
+                ]
             }
         ]
     });
@@ -75,107 +82,34 @@ const scanQueueWorker = new Worker('scanQueue', async job => {
         case 'removeUniques':
             console.debug('Removing unique files...');
             const filesData = Object.values(await job.getChildrenValues())[0];
-            
-            if (!Array.isArray(filesData)) {
-                throw new Error('Files data is not in the expected format');
-            }
-            
-            // const duplicateGroups = filesData.filter(([_, group]) => group.length > 1);
-            // duplicateGroups.forEach(([size, files]) => {
-            //     return files;
-            // });
+            // const filesData = job.childrenValues?.[0] 
 
             for (const [size, files] of filesData) {
                 if (files.length > 1) {
+                    console.debug('hashing files...');
                     // const hashedFiles = await scan.hashFilesInIntervals(size, files);
                 } else {
-                    console.debug('Adding saveToRedis job with parent:', job.id, 'queue:', job.queueName);
-                    await scanQueue.add('saveToRedis', 
-                        { size, files },
-                        { parent: { id: job.id, queue: 'scanQueue' } }  // Use explicit queue name
-                    );
+                    // await scanQueue.add('saveToRedis', { size, files }, { lifo: true });
+                    return size, files;
                 }
             }
-
-            // filesData.forEach(([size, files]) => {
-            //     if (files.length > 1) {
-            //         console.debug(`files.length > 1: ${files.length}, size: ${size}`);
-            //     } else {
-            //         console.debug(`files.length = 1: ${files.length}, size: ${size}`);
-            //     }
-            // })
-            // return
-        
-            //     // Flow2: job3 should run after job4 in queue1
-            //     await flow.add({
-            //       name: 'job4',
-            //       queueName: 'queue2',
-            //       data: { step: 4 },
-            //       children: [
-            //         {
-            //           name: 'job3',
-            //           queueName: 'queue1',
-            //           data: { step: 5 }
-            //         }
-            //       ]
-            //     });
-            //   } else {
-            //     console.log('Adding job3 to flow1');
-                
-            //     // Add job3 to flow1 (queue1)
-            //     await flow.add({
-            //       name: 'job3',
-            //       queueName: 'queue1',
-            //       data: { step: 3 },
-            //       parent: {
-            //         id: job.id,
-            //         queue: job.queue
-            //       }
-            //     });
-            //   }
-            return true;
+            // return true;
         
             
 
         case 'saveToRedis':
             console.debug('Saving duplicate groups to Redis...');
-            const { size, files } = job.data;
+            const { size, files } = Object.values(await job.getChildrenValues())[0];
+            // const { size, files } = job.data;
             console.debug(`Saving size: ${size}, files: ${files}`);
             for (const file of files) {
                 file.size = size;
                 await fileRepository.save(file.ino, file);
             }
-            // const groups = Object.values(await job.getChildrenValues())[0];
-            // console.debug('Groups:', groups);
-            // // Process each size group and save to Redis
-            // for (const [size, files] of groups) {
-            //     // Hash the files in the group if they have the same size
-            //     const hashedFiles = await scan.hashFilesInIntervals(size, files);
-                
-            //     // Save each file's metadata to Redis
-            //     for (const file of hashedFiles) {
-            //         await fileRepository.save(file.ino, file);
-            //     }
-            // }
             return true;
     }
 }, queueConfig);
 
-
-
-//   const job2Worker = new Worker('main-queue', async (job) => {
-//     if (job.name === 'job2' && !job.returnvalue.condition) {
-//       // Only add job2b if condition is false
-//       await flowProducer.add({
-//         name: 'job2b',
-//         queueName: 'scanQueue',
-//         data: {},
-//         children: [] // job3 is already scheduled
-//       });
-//     }
-//   }, { autorun: false });
-
-// Handle worker completion events
 scanQueueWorker.on('completed', job => {
     console.debug(`Job ${job.name} completed.`);
 });
