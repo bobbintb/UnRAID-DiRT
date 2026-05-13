d20b1ee8bd1832669bd68777f692b9ee3aca1bf4
bobbintb <bobbintb@gmail.com>
2026-05-12 20:03:00 -0600

    Update post-install.sh

--- .plugin/post-install.sh
+++ .plugin/post-install.sh
@@ -5,31 +5,3 @@ echo "Installing NodeJS libraries..."
 echo "-----------------------------------------------------------"
 cd "/usr/local/emhttp/plugins/bobbintb.system.dirt/"
 npm install
-
-echo "-----------------------------------------------------------"
-echo "Setting Valkey permissions..."
-echo "-----------------------------------------------------------"
-chmod +x /etc/rc.d/rc.valkey
-
-echo "-----------------------------------------------------------"
-echo "Configuring Valkey..."
-echo "-----------------------------------------------------------"
-# Edits Valkey config to load Redisearch (idempotent)
-awk -v mod="loadmodule /usr/bin/valkey-modules/redisearch.so" '
-    $0 == mod { exists=1 } 
-    /^# loadmodule/ { last=NR } 
-    { lines[NR]=$0 } 
-    END { 
-        for(i=1;i<=NR;i++) { 
-            print lines[i]; 
-            if(!exists && i==last) print mod 
-        } 
-    }' /etc/valkey/valkey.conf > temp.conf && mv temp.conf /etc/valkey/valkey.conf
-
-# 1. Comment out the bind directive if it is active
-# This looks for a line starting with "bind" and adds a # at the start.
-sed -i 's/^bind 127.0.0.1 -::1/# &/' /etc/valkey/valkey.conf
-
-# 2. Change protected-mode from yes to no
-# This ensures the state is explicitly "no".
-sed -i 's/^protected-mode yes/protected-mode no/' /etc/valkey/valkey.conf
